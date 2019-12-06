73b3c45d0679e73322617cd89fbbb2012f50ff6e
hdevarajan92 <hdevarajan@hawk.iit.edu>
2019-12-06 11:38:13 -0600

    added changing of hash function in unordered_map and set.

--- include/basket/common/configuration_manager.h
+++ include/basket/common/configuration_manager.h
@@ -83,12 +83,13 @@ namespace basket{
           file_load.lock();
           SERVER_LIST=std::vector<CharStruct>();
           fstream file;
-          int retry=-1;
+          file.open(SERVER_LIST_PATH.c_str(), ios::in);
+          /*int retry=-1;
           do {
               retry++;
               usleep(10);
               file.open(SERVER_LIST_PATH.c_str(), ios::in);
-          }while(file.is_open() && retry < 5);
+          }while(file.is_open() && retry < 5);*/
           if (file.is_open()) {
               std::string file_line;
 
