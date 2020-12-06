3669ea94ca6aaa19064949037d27eb6d687ea5ef
hariharan devarajan <mani.hariharan@gmail.com>
2020-12-06 17:43:46 -0600

    added hcl installation

--- ci/install_hcl.sh
+++ ci/install_hcl.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+THALLIUM_VERSION=0.8.3
+MERCURY_VERSION=2.0.0
+MPICH_VERSION=3.2.1
+RPCLIB_VERSION=2.2.1
+BOOST_VERSION=1.74.0
+GCC_VERSION=9.3.0
+
+GCC_SPEC="gcc@${GCC_VERSION}"
+MPICH_SPEC="mpich@${MPICH_VERSION}%${GCC_SPEC}"
+MERCURY_SPEC="mercury@${MERCURY_VERSION}%${GCC_SPEC}"
+THALLIUM_SPEC="mochi-thallium~cereal@${THALLIUM_VERSION}%${GCC_SPEC}"
+RPCLIB_SPEC="rpclib@${RPCLIB_VERSION}%${GCC_SPEC}"
+BOOST_SPEC="boost@${BOOST_VERSION}%${GCC_SPEC}"
+
 LOCAL=local
-INSTALL_DIR="${HOME}/${LOCAL}"
+INSTALL_DIR="${HOME}/${LOCAL}/install"
 SPACK_DIR=${INSTALL_DIR}/spack
-INSTALL_DIR=${SPACK_DIR}/var/spack/environments/hcl/.spack-env/view
+rm -r ${INSTALL_DIR}
+mkdir -p ${INSTALL_DIR}
+
+set +x
+. ${SPACK_DIR}/share/spack/setup-env.sh
+set -x
+
+spack view symlink ${INSTALL_DIR} ${GCC_SPEC} ${MPICH_SPEC} ${MERCURY_SPEC} ${THALLIUM_SPEC} ${RPCLIB_SPEC} ${BOOST_SPEC}
+
 mkdir build
 pushd build
 
