2a1ae898087eae59b11fd0aae1709d7a609f6250
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-12-07 23:03:04 -0600

    fixed Run case

--- ci/run_hcl.sh
+++ ci/run_hcl.sh
@@ -7,19 +7,19 @@ if [ "${HCL_ENABLE_RPCLIB}" = "ON" ]; then
     pushd test
     # TODO(chogan): Run tests via ctest and enable Thallium tests
     echo "Testing ordered map"
-    mpiexec -n 2 ./map_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./map_test || exit 1
     echo "Testing multimap"
-    mpiexec -n 2 ./multimap_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./multimap_test || exit 1
     echo "Testing priority queue"
-    mpiexec -n 2 ./priority_queue_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./priority_queue_test || exit 1
     echo "Testing queue"
-    mpiexec -n 2 ./queue_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./queue_test || exit 1
     echo "Testing set"
-    mpiexec -n 2 ./set_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./set_test || exit 1
     echo "Testing unordered map test"
-    mpiexec -n 2 ./unordered_map_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./unordered_map_test || exit 1
     echo "Testing unordered map string test"
-    mpiexec -n 2 ./unordered_map_string_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./unordered_map_string_test || exit 1
     popd
 fi
 
@@ -27,19 +27,19 @@ if [ "${HCL_ENABLE_THALLIUM_TCP}" = "ON" ]; then
     pushd test
     # TODO(chogan): Run tests via ctest and enable Thallium tests
     echo "Testing ordered map"
-    mpiexec -n 2 ./map_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./map_test || exit 1
     echo "Testing multimap"
-    mpiexec -n 2 ./multimap_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./multimap_test || exit 1
     echo "Testing priority queue"
-    mpiexec -n 2 ./priority_queue_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./priority_queue_test || exit 1
     echo "Testing queue"
-    mpiexec -n 2 ./queue_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./queue_test || exit 1
     echo "Testing set"
-    mpiexec -n 2 ./set_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./set_test || exit 1
     echo "Testing unordered map test"
-    mpiexec -n 2 ./unordered_map_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./unordered_map_test || exit 1
     echo "Testing unordered map string test"
-    mpiexec -n 2 ./unordered_map_string_test || exit 1
+    ${INSTALL_DIR}/bin/mpiexec -n 2 ./unordered_map_string_test || exit 1
     popd
 fi
 popd
